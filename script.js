37f25abed3314c574426cdc1cf486cca5a062de5
adimidania <ja_adimi@esi.dz>
2024-01-13 16:53:28 +0100

    Cleaned the code and add keyboards

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let currentResult = 0;
+let currentOperand = 0;
+let currentOperator = null;
+
+function roundResult(a) {
+    return Math.round(a*1000)/1000;
+}
+
 function add(a, b) {
     return a + b;
 }
@@ -23,9 +31,13 @@ function operate(a, b, op) {
         return add(Number(a),Number(b));
     } else if (op === '-') {
         return substract(Number(a),Number(b));
-    } else if (op === '*') { 
+    } else if (op === '×') { 
         return multiply(Number(a),Number(b));
-    } else if (op === '/') { 
+    } else if (op === '÷') { 
+        if (b == 0) {
+            alert("You can't divide by 0!");
+            return 0;
+        }
         return divide(Number(a),Number(b));
     } else if (op === '%') { 
         return modulo(Number(a),Number(b));
@@ -34,69 +46,118 @@ function operate(a, b, op) {
     }
 }
 
-let currentResult = 0;
-let currentOperand = 0;
-let currentOperator = undefined;
-
-const displayOperand = document.querySelector('#display #current');
-const displayResult = document.querySelector('#display #result');
-const digits = document.querySelectorAll('.digit');
-const operators = document.querySelectorAll('.operator');
-const equals = document.querySelector('#equals');
-const clear = document.querySelector('#clear');
-const sign = document.querySelector('#sign');
-const decimal = document.querySelector('#decimal');
-
-digits.forEach((digit) => {
-    digit.addEventListener('click', function(e) {
-        currentOperand += digit.textContent;
-        displayOperand.textContent = Number(currentOperand);
-        console.log(currentOperand);
-    })
-})
-
-operators.forEach((operator) => {
-    operator.addEventListener('click', function(e) {
-        if (currentOperator == undefined) {
-            currentResult = Number(currentOperand);
-            currentOperand = 0;
-        } else {
-            currentResult = operate(currentResult, currentOperand, currentOperator);
-            currentOperand = 0;
-        }
-        currentOperator = operator.textContent;
-        displayResult.textContent = currentResult + currentOperator;
-        displayOperand.textContent = Number(currentOperand);
-    })
-})
-
-sign.addEventListener('click', function() {
-    currentOperand = operate('-1', currentOperand, '*');
+function appendDigit(digit) {
+    currentOperand += digit;
     displayOperand.textContent = Number(currentOperand);
-})
+}
 
-decimal.addEventListener('click', function() {
+function setOperator(operator) {
+    if (currentOperator == null) {
+        currentResult = Number(currentOperand);
+        currentOperand = 0;
+    } else {
+        currentResult = operate(currentResult, currentOperand, currentOperator);
+        currentOperand = 0;
+    }
+    currentOperator = operator;
+    displayResult.textContent = currentResult + currentOperator;
+    displayOperand.textContent = Number(currentOperand);
+}
+
+function changeSign() {
+    currentOperand = operate('-1', currentOperand, '×');
+    displayOperand.textContent = Number(currentOperand);
+}
+
+function appendDecimal() {
     if (!String(currentOperand).includes('.')) {
         currentOperand =  Number(currentOperand) + '.'
         displayOperand.textContent = currentOperand;
     }
-})
+}
 
-equals.addEventListener('click', function() {
-    if (currentOperator != undefined) {
+function evaluate() {
+    if (currentOperator != null) {
         currentResult = operate(currentResult, currentOperand, currentOperator);
         displayOperand.textContent = currentResult;
         displayResult.textContent = '';
         currentResult = 0;
         currentOperand = 0;
-        currentOperator = undefined;
+        currentOperator = null;
     } 
-})
+}
 
-clear.addEventListener('click', function() {
+function clearCalculator() {
     currentResult = 0;
     currentOperand = 0;
-    currentOperator = undefined;
+    currentOperator = null;
     displayOperand.textContent = currentResult;
     displayResult.textContent = '';
+}
+
+function handleKeyboardInput(e) {
+    if (e.key >= 0 && e.key <= 9) {
+        appendDigit(e.key);
+    }
+    else if (e.key === '.') {
+        appendDecimal();
+    }
+    else if (e.key === '=' || e.key === 'Enter') {
+        evaluate()
+    }
+    else if (e.key === 'Escape') {
+        clear()
+    }
+    else if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/') {
+        setOperator(convertOperator(e.key))
+    } else {
+        alert("Invalid input!");
+    }
+}
+
+function convertOperator(keyboardOperator) {
+    if (keyboardOperator === '/') return '÷'
+    if (keyboardOperator === '*') return '×'
+    if (keyboardOperator === '-') return '−'
+    if (keyboardOperator === '+') return '+'
+  }
+
+const displayOperand = document.querySelector('#display #current');
+const displayResult = document.querySelector('#display #result');
+const digitButtons = document.querySelectorAll('.digit');
+const operatorButtons = document.querySelectorAll('.operator');
+const equalsBtn = document.querySelector('#equals');
+const clearBtn = document.querySelector('#clear');
+const signBtn = document.querySelector('#sign');
+const decimalBtn = document.querySelector('#decimal');
+
+window.addEventListener('keydown', handleKeyboardInput);
+digitButtons.forEach((digit) => {
+    digit.addEventListener('click', function(e) {
+        appendDigit(digit.textContent);
+    })
 })
+
+operatorButtons.forEach((operator) => {
+    operator.addEventListener('click', function(e) {
+        setOperator(operator.textContent);
+    })
+})
+
+signBtn.addEventListener('click', function() {
+    changeSign();
+})
+
+decimalBtn.addEventListener('click', function() {
+    appendDecimal();
+})
+
+equalsBtn.addEventListener('click', function() {
+    evaluate();
+})
+
+clearBtn.addEventListener('click', function() {
+    clearCalculator();
+})
+
+
